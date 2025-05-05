976b6bb9e706aad3e60d8493fd990dd2eec3bd55
muntazir0059@gmail.com <muntazir0059@gmail.com>
2025-05-05 13:04:14 +0530

    updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -8,25 +8,25 @@ function Slider() {
   return (
     <Carousel>
       <Carousel.Item>
-        <img className="d-block w-100" src={CementPlant} alt="First slide" style={{ height: "35vh", objectFit: "cover"}}/>
+        <img className="d-block w-100" src={CementPlant} alt="First slide" style={{ height: "500px", objectFit: "cover"}}/>
         <Carousel.Caption>
           <h3>Cement Plant</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={Industries} alt="First slide" style={{ height: "35vh", objectFit: "cover"}}/>
+        <img className="d-block w-100" src={Industries} alt="First slide" style={{ height: "500px", objectFit: "cover"}}/>
         <Carousel.Caption>
           <h3>Industries</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={Institution} alt="First slide" style={{ height: "35vh", objectFit: "cover"}}/>
+        <img className="d-block w-100" src={Institution} alt="First slide" style={{ height: "500px", objectFit: "cover"}}/>
         <Carousel.Caption>
           <h3>Institution</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={Construction} alt="Third slide" style={{ height: "35vh", objectFit: "cover" }}/>
+        <img className="d-block w-100" src={Construction} alt="Third slide" style={{ height: "500px", objectFit: "cover" }}/>
         <Carousel.Caption >
           <h3>Construction</h3>
         </Carousel.Caption>
